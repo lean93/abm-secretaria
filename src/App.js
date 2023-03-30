7ea980350b4c6f10b1c05724257eba8a278c3e6e
lean93 <leandro.rodriguez@despegar.com>
2023-03-30 20:06:34 -0300

    start proyect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Alert from 'react-bootstrap/Alert';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Alert key='success' variant='success'>
+      Pronto aca vas a poder cargar todo Agustin
+    </Alert>    
+     </>
+    );
 }
 
 export default App;
