ab822143a3c05db1be2eb63cf51eb8ff482130b1
lean93 <leandro.rodriguez@despegar.com>
2023-03-30 20:28:26 -0300

    add axios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
+import axios from 'axios';
+
+import React, { useState } from 'react';
+
 
 function App() {
+
+  const [data, setData] = useState("Vacio");
+
+
+  const getData =()=>{
+    axios.get("/clientes").then(response=>{
+      console.log(response);
+      setData(JSON.stringify(response.data))
+    }).catch(error=>{
+
+    });
+  }
+
   return (
     <>
     <Alert key='success' variant='success'>
       Pronto aca vas a poder cargar todo Agustin
-    </Alert>    
+    </Alert> 
+    <Button onClick={getData} variant="primary">Buscar</Button>
+    <h4>{data}</h4>
      </>
     );
 }
